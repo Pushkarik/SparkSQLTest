6cc0e0f2fa78a7167614524005e3b0718fa88553
Pushkarik <pushkarev.e@gmail.com>
2017-12-19 13:00:55 -0500

    Third

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,60 +29,60 @@ public class Main {
         // Start Spark on local
         SparkSession spark = SparkSession.builder().appName("Simple Application").master("local").getOrCreate();
 
-        if (typeMode==1) {
+        try {
+            if (typeMode == 1) {
 
-            // STAGE I
-            // * Generate 2 parquet-file from one json-file
-            Dataset<Row> events = spark.read().json("data/events.json");
-            events.printSchema(); // show schema
+                // STAGE I
+                // * Generate 2 parquet-file from one json-file
+                Dataset<Row> events = spark.read().json("data/events.json");
+                events.printSchema(); // show schema
 
-            // Create view for execute SQL later...
-            events.createOrReplaceTempView("events");
+                // Create view for execute SQL later...
+                events.createOrReplaceTempView("events");
 
-            spark.sql("SELECT cast(_t as timestamp) as time, _p as email, device_type FROM events WHERE _n = 'app_loaded'").write().mode(SaveMode.Overwrite).parquet("/home/user/events/app_loaded/app_loaded.parquet"); //"data/app_loaded.parquet"
-            spark.sql("SELECT cast(_t as timestamp) as time, _p as email, channel FROM events WHERE _n = 'registered'").write().mode(SaveMode.Overwrite).parquet("/home/user/events/registered/registered.parquet"); // "data/registred.parquet"
+                spark.sql("SELECT cast(_t as timestamp) as time, _p as email, device_type FROM events WHERE _n = 'app_loaded'").write().mode(SaveMode.Overwrite).parquet("/home/user/events/app_loaded/app_loaded.parquet"); //"data/app_loaded.parquet"
+                spark.sql("SELECT cast(_t as timestamp) as time, _p as email, channel FROM events WHERE _n = 'registered'").write().mode(SaveMode.Overwrite).parquet("/home/user/events/registered/registered.parquet"); // "data/registred.parquet"
+            } else if (typeMode == 2) {
+
+                // STAGE II
+                // * SQL under parquet-files
+                // * Print into console data
+                Dataset<Row> parquetFileApp = spark.read().parquet("/home/user/events/app_loaded/app_loaded.parquet");
+                Dataset<Row> parquetFileReg = spark.read().parquet("/home/user/events/registered/registered.parquet");
+
+                parquetFileApp.createOrReplaceTempView("parquetApp");
+                parquetFileReg.createOrReplaceTempView("parquetReg");
+
+                // these are the guys who downloaded the application one week after registration
+                String sqlText = "SELECT count(*) as cnt" +
+                        "  FROM parquetReg t1, " +
+                        "       (select email, min(time) as time " + // only first download app
+                        "          from parquetApp " +
+                        "         group by email) t2 " +
+                        " WHERE t1.email=t2.email" +
+                        "   and datediff(t2.time,t1.time) <= 7" +
+                        "   and datediff(t2.time,t1.time) >= 0";
+                Dataset<Row> namesDF = spark.sql(sqlText);
+                long cntGoodGuys = namesDF.select(col("cnt")).first().getLong(0);
+
+                // Count of All registered users
+                sqlText = "SELECT count(email) as cnt" +
+                        "  FROM parquetReg ";
+                Dataset<Row> namesDF2 = spark.sql(sqlText);
+                long cntAll = namesDF2.select(col("cnt")).first().getLong(0);
+
+                // Print results
+                float prc = ((float) cntGoodGuys) / ((float) cntAll) * 100;
+                System.out.printf("Percentage of app downloads: %.2f", prc);
+                System.out.println("%");
+            } else {
+                System.out.println("ERROR! Unknown typeMode! Check the program code!");
+                System.exit(1);
+            }
         }
-        else if (typeMode==2)
-        {
-
-            // STAGE II
-            // * SQL under parquet-files
-            // * Print into console data
-            Dataset<Row> parquetFileApp = spark.read().parquet("/home/user/events/app_loaded/app_loaded.parquet");
-            Dataset<Row> parquetFileReg = spark.read().parquet("/home/user/events/registered/registered.parquet");
-
-            parquetFileApp.createOrReplaceTempView("parquetApp");
-            parquetFileReg.createOrReplaceTempView("parquetReg");
-
-            // these are the guys who downloaded the application one week after registration
-            String sqlText = "SELECT count(*) as cnt" +
-                             "  FROM parquetReg t1, " +
-                             "       (select email, min(time) as time " + // only first download app
-                             "          from parquetApp " +
-                             "         group by email) t2 " +
-                             " WHERE t1.email=t2.email" +
-                             "   and datediff(t2.time,t1.time) <= 7" +
-                             "   and datediff(t2.time,t1.time) >= 0";
-            Dataset<Row> namesDF = spark.sql(sqlText);
-            long cntGoodGuys = namesDF.select(col("cnt")).first().getLong(0);
-
-            // Count of All registered users
-            sqlText = "SELECT count(email) as cnt" +
-                      "  FROM parquetReg ";
-            Dataset<Row> namesDF2 = spark.sql(sqlText);
-            long cntAll = namesDF2.select(col("cnt")).first().getLong(0);
-
-            // Print results
-            float prc = ((float) cntGoodGuys) / ((float) cntAll) * 100;
-            System.out.printf("Percentage of app downloads: %.2f", prc);
-            System.out.println("%");
+        finally {
+            // SparkSession stop
+            spark.stop();
         }
-        else
-        {
-            System.out.println("ERROR! Unknown typeMode! Check the program code!");
-            System.exit(1);
-        }
-        // SparkSession stop
-        spark.stop();
     }
 }
